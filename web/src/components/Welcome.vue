f62039d53d13adc3c6dce4f5b405c3a0069b7f33
liam-armstrong <liam.armstrong124@gmail.com>
2020-04-05 22:47:38 -0700

    Linting

--- web/src/components/Welcome.vue
+++ web/src/components/Welcome.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="welcome text-center">
-		<h2>Welcome to Trains'R'Us</h2><br>
-		<img src="@/assets/train.png" alt="train wallpaper"><br>
-		<p>This is a database management website for a railway corperation</p><br>
-		<p>For HR, manage all employees in <b>'Workers'</b><br>
-		For maintenance employees, view track status in <b>'Tracks'</b><br>
-		For on-train employees, view shifts and check tickets in <b>'Train'</b></p>
-    </div>
+  <div class="welcome text-center">
+    <h2>Welcome to Trains'R'Us!</h2><br>
+    <img
+      src="@/assets/train.png"
+      alt="train wallpaper"
+    ><br>
+    <p>This is a database management website for a railway corperation</p><br>
+    <p>
+      For HR, manage all employees in <b>'Workers'</b><br>
+      For maintenance employees, view track status in <b>'Tracks'</b><br>
+      For on-train employees, view shifts and check tickets in <b>'Train'</b>
+    </p>
+  </div>
 </template>
 
 <script>
